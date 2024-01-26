72cf1a9c1a8d66612e816ceac665bb7e28321c05
CaRn15 <kaapo.karppinen.kaapo@gmail.com>
2024-01-26 15:39:59 +0200

    part3

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const morgan = require('morgan');
 const app = express()
 const cors = require('cors')
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(morgan('tiny'));
+app.use(express.static('dist'))
 
 app.use(morgan((tokens, req, res) => {
   return [
